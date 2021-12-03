701f5c26d2fda4cf130ce302e7fa24d99760dff5
Erik Røed <post@erikroed.no>
2021-12-03 01:55:35 +0100

    Solve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,49 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var hit = 0
+
+        input.forEachIndexed { index, element ->
+            if (index == 0) return@forEachIndexed
+
+            val previousNumber = input.elementAt(index.minus(1)).toInt()
+            val currentNumber = element.toInt()
+            if (currentNumber > previousNumber) {
+                hit++
+            }
+        }
+
+        return hit
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var hit = 0
+
+        input.forEachIndexed { index, _ ->
+            val toPosition = index.plus(3)
+            val secondFromPosition = index.plus(1)
+            val secondToPosition = toPosition.plus(1)
+
+            val validPosition =
+                listOf(index, toPosition, secondFromPosition, secondToPosition).all { it <= input.size }
+
+            if (validPosition) {
+                val firstChunk = input.subList(index, toPosition).sumOf { it.toInt() }
+                val secondChunk = input.subList((index + 1), (toPosition + 1)).sumOf { it.toInt() }
+
+                if (secondChunk > firstChunk) {
+                    hit++
+                }
+            }
+
+        }
+
+        return hit
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
