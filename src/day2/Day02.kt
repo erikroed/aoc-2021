4f569ac196703d3771c579ad241d7c6c66e88ef2
Erik Røed <post@erikroed.no>
2021-12-05 02:21:38 +0100

    Day 2 part 2 solution

--- src/day2/Day02.kt
+++ src/day2/Day02.kt
@@ -22,13 +22,31 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var horizontal = 0
+        var depth = 0
+        var aim = 0
+
+        input.forEach {
+            val operation = it.substringBefore(" ").lowercase()
+            val value = it.substringAfter(" ").toInt()
+
+            when (operation) {
+                "down" -> aim += value
+                "up" -> aim -= value
+                "forward" -> {
+                    horizontal += value
+                    depth += aim * value
+                }
+            }
+        }
+
+        return horizontal * depth
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day2/Day02_test")
     check(part1(testInput) == 150)
-    check(part2(testInput) == 6)
+    check(part2(testInput) == 900)
 
     val input = readInput("day2/Day02")
     println(part1(input))
