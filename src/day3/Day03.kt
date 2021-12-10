819ca36356d6e2155675320415d19b85dbcdaf9a
Erik Røed <post@erikroed.no>
2021-12-10 21:20:17 +0100

    Use enum as suggested in Jetbrains solution

--- src/day3/Day03.kt
+++ src/day3/Day03.kt
@@ -19,33 +19,26 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        fun oxyRating(): String {
+        fun rating(type: RatingType): String {
             val columns = input[0].indices
             var candidates = input
             for (column in columns) {
                 val (zeroes, ones) = candidates.countBitsInColumn(column)
                 val mostCommon = if (zeroes > ones) '0' else '1'
-                candidates = candidates.filter { it[column] == mostCommon }
+                candidates = candidates.filter {
+                    when (type) {
+                        RatingType.OXYGEN -> it[column] == mostCommon
+                        RatingType.CO2 -> it[column] != mostCommon
+                    }
+                }
+
                 if (candidates.size == 1) break
             }
 
             return candidates.single()
         }
 
-        fun co2Rating(): String {
-            val columns = input[0].indices
-            var candidates = input
-            for (column in columns) {
-                val (zeroes, ones) = candidates.countBitsInColumn(column)
-                val mostCommon = if (zeroes > ones) '0' else '1'
-                candidates = candidates.filter { it[column] != mostCommon }
-                if (candidates.size == 1) break
-            }
-
-            return candidates.single()
-        }
-
-        return oxyRating().toInt(2) * co2Rating().toInt(2)
+        return rating(RatingType.OXYGEN).toInt(2) * rating(RatingType.CO2).toInt(2)
     }
 
     // test if implementation meets criteria from the description, like:
@@ -70,5 +63,7 @@ private fun List<String>.countBitsInColumn(column: Int): BitCount {
 }
 
 private fun String.invertBinaryString() = this.asIterable().joinToString("") { if (it == '0') "1" else "0" }
+
+private enum class RatingType { OXYGEN, CO2 }
 
 data class BitCount(val zeroes: Int, val ones: Int)
